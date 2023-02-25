3f71707b59e13bf84039f249462aa00f837374f4
Chris L <chrlew082@gmail.com>
2023-02-25 01:47:02 -0700

    first commit

---
+++ src/main/java/me/experimental/Main.java
@@ -0,0 +1,10 @@
+package me.experimental;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hey there :P");
+    }
+}
